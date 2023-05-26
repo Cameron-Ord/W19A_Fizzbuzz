b66c58e0a8b37006b716cb15078c659fbb5434f7
NuckenMcFuggets <cameron.ian.ronald.ord@Outlook.com>
2023-05-26 17:03:54 -0600

    fizzbuzz'd

---
+++ python/fizzbuzz.py
@@ -0,0 +1,39 @@
+
+def fizzbuzz(arg1):
+    
+    if(arg1 % 3 == 0 and arg1 % 5 == 0):
+        print ('FizzBuzz', '-', arg1)
+        print()
+        return True 
+    if(arg1 % 3 == 0):
+        print ('Fizz', '-', arg1)
+        print()
+        return True
+    if(arg1 % 5 == 0):
+        print ('Buzz', '-', arg1)
+        print()
+        return True
+
+
+    
+    
+    
+    
+fizzbuzz(30)
+fizzbuzz(5)
+fizzbuzz(3)
+fizzbuzz(45)
+fizzbuzz(6)
+fizzbuzz(9)
+fizzbuzz(60)
+fizzbuzz(75)
+fizzbuzz(90)
+fizzbuzz(66)
+fizzbuzz(78)
+fizzbuzz(96)
+fizzbuzz(50)
+fizzbuzz(35)
+fizzbuzz(99)
+fizzbuzz(25)
+
+
