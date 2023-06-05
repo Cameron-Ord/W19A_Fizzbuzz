68af9f595ad038bb63e65a085f1dea7931fb6e12
NuckenMcFuggets <cameron.ian.ronald.ord@Outlook.com>
2023-06-05 15:17:14 -0600

    Fizzbuzz

--- python/fizzbuzz.py
+++ python/fizzbuzz.py
@@ -3,37 +3,21 @@ def fizzbuzz(arg1):
     
     if(arg1 % 3 == 0 and arg1 % 5 == 0):
         print ('FizzBuzz', '-', arg1)
-        print()
-        return True 
+
     if(arg1 % 3 == 0):
         print ('Fizz', '-', arg1)
-        print()
-        return True
+
     if(arg1 % 5 == 0):
         print ('Buzz', '-', arg1)
-        print()
-        return True
 
 
+
     
     
     
-    
-fizzbuzz(30)
-fizzbuzz(5)
-fizzbuzz(3)
-fizzbuzz(45)
-fizzbuzz(6)
-fizzbuzz(9)
-fizzbuzz(60)
-fizzbuzz(75)
-fizzbuzz(90)
-fizzbuzz(66)
-fizzbuzz(78)
-fizzbuzz(96)
-fizzbuzz(50)
-fizzbuzz(35)
-fizzbuzz(99)
-fizzbuzz(25)
+nums = [9921, 10505, 999, 775, 779, 900, 333, 69, 3, 99, 600, 750, 6030120, 70, 72, 102, 96]
+
+for x in nums:
+    fizzbuzz(x)
 
 
